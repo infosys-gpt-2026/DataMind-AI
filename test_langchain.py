eef5c181d7cc9ae545e918352c78bd7269b62b7b
NITIN GUPTA <88649588+nitin28061999@users.noreply.github.com>
2026-09-06 12:57:42 +0530

    test_langchain

---
+++ test_langchain.py
@@ -0,0 +1,21 @@
+﻿import os
+from dotenv import load_dotenv # pyright: ignore[reportMissingImports]
+from langchain_google_genai import ChatGoogleGenerativeAI # pyright: ignore[reportMissingImports]
+
+# Load environment variables
+load_dotenv()
+
+# Initialize Gemini
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    google_api_key=os.getenv("GOOGLE_API_KEY"),
+    temperature=0.3
+)
+
+# Ask a question
+response = llm.invoke(
+    "Explain what a Data Analyst does in simple words."
+)
+
+print("\n🤖 AI Response:\n")
+print(response.content)
